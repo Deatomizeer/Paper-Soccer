e4e64d87d80df083adaa2d1256e5f4144087a229
Deatomizeer <56254654+Deatomizeer@users.noreply.github.com>
2021-08-26 14:44:11 +0200

    made a rudimentary bot
    After player moves, a very simple program moves back, trying to score a goal. Sometimes it makes Unity crash, which is unintentional.

--- Assets/BoardManager.cs
+++ Assets/BoardManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,8 +14,8 @@ public class BoardManager : MonoBehaviour
     InterjectionAbstract[,] boardLogic = new InterjectionAbstract[boardHeight, boardWidth];
     GameObject[,] boardView = new GameObject[boardHeight, boardWidth];
     // Ball position.
-    int xBall = (boardWidth-1)/2;
-    int yBall = (boardHeight-1)/2;
+    int xBall = (boardWidth - 1) / 2;
+    int yBall = (boardHeight - 1) / 2;
     // Moves that have already been made in this game.
     public class GameMove
     {
@@ -29,14 +30,16 @@ public class BoardManager : MonoBehaviour
     List<GameMove> pastMoves = new List<GameMove>();
     List<GameObject> pastMovesView = new List<GameObject>(); // For ease of restarting the game.
     List<GameMove> boardBoundaries = new List<GameMove>();   // Moving the ball along the boundary line is not allowed.
+    // Current player, used to prevent unauthorised movement during the opponent's turn.
+    GoalAffiliation currentPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         // Create the board.
-        for(int i = 0; i < boardHeight; i++)
+        for (int i = 0; i < boardHeight; i++)
         {
-            for(int j = 0; j < boardWidth; j++ )
+            for (int j = 0; j < boardWidth; j++)
             {
                 boardLogic[i, j] = CreateNextCell(i, j);
             }
@@ -54,17 +57,17 @@ public class BoardManager : MonoBehaviour
         // Correct the camera's position.
         Camera.main.gameObject.GetComponent<CenterCamera>().CenterCameraOnBoard(boardWidth, boardHeight);
         // Create field boundaries.
-        for(int j=0; j < boardWidth-1; j++)
+        for (int j = 0; j < boardWidth - 1; j++)
         {
             // Horizontal boundaries.
             boardBoundaries.Add(new GameMove(boardView[0, j], boardView[0, j + 1]));
-            boardBoundaries.Add(new GameMove(boardView[boardHeight-1, j], boardView[boardHeight-1, j + 1]));
+            boardBoundaries.Add(new GameMove(boardView[boardHeight - 1, j], boardView[boardHeight - 1, j + 1]));
             if (j != (boardWidth - 1) / 2)
             {
                 // Vertical boundaries between the two horizontal ones, except where goal is.
                 boardBoundaries.Add(new GameMove(boardView[0, j], boardView[1, j]));
-                boardBoundaries.Add(new GameMove(boardView[boardHeight-1, j], boardView[boardHeight-2, j]));
-                if ( j != ((boardWidth - 1) / 2) - 1)
+                boardBoundaries.Add(new GameMove(boardView[boardHeight - 1, j], boardView[boardHeight - 2, j]));
+                if (j != ((boardWidth - 1) / 2) - 1)
                 {
                     // Horizontal boundaries, but closer to the middle.
                     boardBoundaries.Add(new GameMove(boardView[1, j], boardView[1, j + 1]));
@@ -72,19 +75,19 @@ public class BoardManager : MonoBehaviour
                     // Diagonal boundaries between the two horizontal rows.
                     boardBoundaries.Add(new GameMove(boardView[0, j], boardView[1, j + 1]));
                     boardBoundaries.Add(new GameMove(boardView[1, j], boardView[0, j + 1]));
-                    boardBoundaries.Add(new GameMove(boardView[boardHeight-1, j], boardView[boardHeight-2, j + 1]));
-                    boardBoundaries.Add(new GameMove(boardView[boardHeight-2, j], boardView[boardHeight-1, j + 1]));
+                    boardBoundaries.Add(new GameMove(boardView[boardHeight - 1, j], boardView[boardHeight - 2, j + 1]));
+                    boardBoundaries.Add(new GameMove(boardView[boardHeight - 2, j], boardView[boardHeight - 1, j + 1]));
                 }
             }
         }
-        for(int i=0; i<boardHeight-1; i++)
+        for (int i = 0; i < boardHeight - 1; i++)
         {
             // Vertical boundaries on left and right.
-            boardBoundaries.Add(new GameMove(boardView[i, 0], boardView[i+1, 0]));
-            boardBoundaries.Add(new GameMove(boardView[i, boardWidth-1], boardView[i + 1, boardWidth - 1]));
+            boardBoundaries.Add(new GameMove(boardView[i, 0], boardView[i + 1, 0]));
+            boardBoundaries.Add(new GameMove(boardView[i, boardWidth - 1], boardView[i + 1, boardWidth - 1]));
         }
         // Render the boundaries as game objects.
-        for(int i = 0; i < boardBoundaries.Count; i++)
+        for (int i = 0; i < boardBoundaries.Count; i++)
         {
             GameMove edge = boardBoundaries[i];
             float edgeX = (edge.firstNode.transform.position.x + edge.secondNode.transform.position.x) * .5f;
@@ -94,6 +97,8 @@ public class BoardManager : MonoBehaviour
         }
         // Copy board boundaries into moves made for ease of checking.
         pastMoves = new List<GameMove>(boardBoundaries);
+        // Player1 (red) starts the game.
+        currentPlayer = GoalAffiliation.PLAYER1;
     }
 
     // Determine what should the cell be based on its position.
@@ -142,11 +147,11 @@ public class BoardManager : MonoBehaviour
                 return new Interjection(InterjectionStatus.FREE, false);
             }
         }
-        
-        
+
+
 
     }
-    
+
     // Find all allowed moves from current ball position. Will probably break if called
     List<GameObject> GetLegalMoves()
     {
@@ -157,17 +162,17 @@ public class BoardManager : MonoBehaviour
         int iterStartY = yBall > 0 ? yBall - 1 : 0;
         int iterEndY = yBall < boardHeight - 1 ? yBall + 2 : boardHeight;
         // Iterate over all neighbors.
-        for(int i = iterStartY; i < iterEndY; i++)
+        for (int i = iterStartY; i < iterEndY; i++)
         {
-            for(int j = iterStartX; j < iterEndX; j++)
+            for (int j = iterStartX; j < iterEndX; j++)
             {
                 // Scoring a goal is always an option (at least with the regular board layout).
-                if(boardLogic[i, j] is Goal)
+                if (boardLogic[i, j] is Goal)
                 {
                     destinations.Add(boardView[i, j]);
                 }
                 // Skip self to prevent stalling the game.
-                else if((boardLogic[i, j] as Interjection).hasBall)
+                else if ((boardLogic[i, j] as Interjection).hasBall)
                 {
                     continue;
                 }
@@ -177,9 +182,9 @@ public class BoardManager : MonoBehaviour
                     GameMove move = new GameMove(boardView[yBall, xBall], boardView[i, j]);
                     GameMove reverseMove = new GameMove(boardView[i, j], boardView[yBall, xBall]);
                     bool hasBeenMade = false;
-                    foreach(GameMove m in pastMoves)
+                    foreach (GameMove m in pastMoves)
                     {
-                        if(
+                        if (
                             (m.firstNode == move.firstNode && m.secondNode == move.secondNode)
                             || (m.firstNode == reverseMove.firstNode && m.secondNode == reverseMove.secondNode)
                         )
@@ -188,7 +193,7 @@ public class BoardManager : MonoBehaviour
                             break;
                         }
                     }
-                    if(!hasBeenMade)
+                    if (!hasBeenMade)
                     {
                         destinations.Add(boardView[i, j]);
                     }
@@ -196,7 +201,7 @@ public class BoardManager : MonoBehaviour
             }
         }
         string d = "Allowed moves: ";
-        foreach(GameObject dest in destinations)
+        foreach (GameObject dest in destinations)
         {
             string tmp = "(" + dest.transform.position.x + ", " + dest.transform.position.y + ") ";
             d += tmp;
@@ -208,12 +213,22 @@ public class BoardManager : MonoBehaviour
     // Try to move the ball to the selected node.
     public void MakeMove(GameObject dest)
     {
+        if (currentPlayer != GoalAffiliation.PLAYER1)
+        {
+            // It's not your turn yet!
+            return;
+        }
         List<GameObject> legalMoves = GetLegalMoves();
+        if (legalMoves.Count == 0)
+        {
+            Stalemate();
+            return;
+        }
         if (legalMoves.Contains(dest))
         {
             // All good, make this move.
             Cell destCell = dest.GetComponent<Cell>();
-            if ( destCell.thisInterjection is Goal )
+            if (destCell.thisInterjection is Goal)
             {
                 ScoreGoal((destCell.thisInterjection as Goal).affiliation);
                 return;
@@ -236,6 +251,12 @@ public class BoardManager : MonoBehaviour
             destCell.spriteRenderer.color = Color.yellow;
             xBall = (int)dest.transform.position.x;
             yBall = (int)dest.transform.position.y;
+            // End turn and let the AI move... or bounce off a wall!
+            if ((boardLogic[yBall, xBall] as Interjection).status == InterjectionStatus.FREE)
+            {
+                currentPlayer = GoalAffiliation.PLAYER2;
+                Invoke("MakeMoveAI", 1);    // Force the program to wait to make the AI feel more "real".
+            }
         }
         //Debug.Log("Ball on (" + xBall.ToString() + ", " + yBall.ToString() + ")");
     }
@@ -245,9 +266,74 @@ public class BoardManager : MonoBehaviour
     {
         Debug.Log(aff.ToString() + " scores a goal!");
     }
-    // Update is called once per frame
-    void Update()
+
+    public void Stalemate()
     {
-        
+        Debug.Log("It's a draw!");
+    }
+
+    // Opponent move.
+    public void MakeMoveAI()
+    {
+        //Debug.Log("I would have made a move right now!");
+        GameObject dest = null;    // The move to make.
+        while ( currentPlayer == GoalAffiliation.PLAYER2 )
+        {
+            List<GameObject> legalMoves = GetLegalMoves();
+            if( legalMoves.Count == 0 )
+            {
+                Stalemate();
+                return;
+            }
+            foreach (GameObject move in legalMoves)
+            {
+                // Check if there's a move that would bring you closer to the goal (lower center of the board).
+                if (move.transform.position.y < yBall)
+                {
+                    dest = move;
+                }
+            }
+            if (legalMoves.Contains(dest))
+            {
+                // All good, make this move.
+                Cell destCell = dest.GetComponent<Cell>();
+                if (destCell.thisInterjection is Goal)
+                {
+                    ScoreGoal((destCell.thisInterjection as Goal).affiliation);
+                    return;
+                }
+                // Create a new edge after this move.
+                GameMove thisMove = new GameMove(boardView[yBall, xBall], dest);
+                pastMoves.Add(thisMove);
+                float edgeX = (thisMove.firstNode.transform.position.x + thisMove.secondNode.transform.position.x) * .5f;
+                float edgeY = (thisMove.firstNode.transform.position.y + thisMove.secondNode.transform.position.y) * .5f;
+                Vector3 edgePos = new Vector3(edgeX, edgeY, 0);
+                pastMovesView.Add(
+                    Instantiate(edgePrefab, edgePos, Quaternion.identity)
+                );
+                // Change the previous cell state to wall for logic purposes.
+                (boardLogic[yBall, xBall] as Interjection).status = InterjectionStatus.WALL;
+                // Move the ball to the destination.
+                (boardLogic[yBall, xBall] as Interjection).hasBall = false;
+                boardView[yBall, xBall].GetComponent<Cell>().spriteRenderer.color = Color.black;    // Wall color.
+                (destCell.thisInterjection as Interjection).hasBall = true;
+                destCell.spriteRenderer.color = Color.yellow;
+                xBall = (int)dest.transform.position.x;
+                yBall = (int)dest.transform.position.y;
+
+                // End the turn on the same condition as player.
+                if ((boardLogic[yBall, xBall] as Interjection).status == InterjectionStatus.FREE)
+                {
+                    currentPlayer = GoalAffiliation.PLAYER1;
+                }
+                else
+                {
+                    IEnumerator e()
+                    {
+                        yield return new WaitForSeconds(1);
+                    }
+                }
+            }
+        }
     }
 }
